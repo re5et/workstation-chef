39ea25b4fd4913b69e23d6b829e32770a47915ac
re5et <re5etsmyth@gmail.com>
2015-02-05 15:22:17 -0800

    Make sure ruby is always available

--- site-cookbooks/workstation/recipes/ruby.rb
+++ site-cookbooks/workstation/recipes/ruby.rb
@@ -1,13 +1,12 @@
 ruby_build_ruby node[:ruby]
 
-template "/etc/profile.d/ruby.sh" do
-  group "root"
-  owner "root"
-  mode "0744"
-  source "workstation-ruby.erb"
-  variables({
-      :ruby => node[:ruby]
-    })
+%w{ /etc/bash.bashrc /etc/profile.d/ruby.sh }.each do |script_path|
+  template script_path do
+    group "root"
+    owner "root"
+    mode "0744"
+    source "workstation-ruby.erb"
+  end
 end
 
 execute "install bundler" do

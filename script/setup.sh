d6cd294e15c8261734614a0ef0da50e665251158
re5et <re5etsmyth@gmail.com>
2015-02-05 15:26:56 -0800

    smoother setup

--- script/setup.sh
+++ script/setup.sh
@@ -1,40 +1,29 @@
 #! env bash
 
+function usage {
+    echo "SERVER=user@server $0 {prepare|cook|all}"
+    exit 1;
+}
+
+test -z $SERVER && echo SET \$SERVER ! && usage
+
+echo
+echo ssh-copy-id...
+echo
 ssh-copy-id $SERVER
 
-# knife solo prepare $SERVER nodes/workstation.json
-knife solo cook $SERVER nodes/workstation.json
-
-# rsync -harv --progress ~/.ssh/ $host:.ssh
-# rsync -harv --progress ~/.mozilla $host:.mozilla
-
-# commands=()
-
-# commands+=('rm -rf Desktop Documents Downloads examples.desktop install.sh Music Pictures Public Templates Videos')
-# commands+=('mkdir ~/code')
-# commands+=('mkdir ~/downloads')
-# commands+=('mkdir ~/tmp')
-# commands+=('mkdir ~/tmpmnt')
-# commands+=('git clone git@github.com:re5et/.emacs.d.git')
-# commands+=('git clone git@github.com:re5et/.dotfiles.git')
-# commands+=('git clone git@github.com:re5et/handy-shell-scripts.git ~/bin')
-# commands+=('cd ~/.dotfiles && git submodule init && git submodule update && git submodule sync')
-# commands+=('unlink .stumpwmrc')
-# commands+=('unlink .keysnail.js')
-# commands+=('unlink .gtkrc-2.0')
-# commands+=('unlink .xscreensaver')
-# commands+=('unlink .zshrc')
-# commands+=('unlink .gitconfig')
-# commands+=('unlink .gitignore')
-# commands+=('rm -rf .stumpwmrc .keysnail.js .gtkrc-2.0 .xscreensaver .zshrc .gitconfig .gitignore')
-# commands+=('ln -s ~/.dotfiles/{.stumpwmrc,.keysnail.js,.gtkrc-2.0,.xscreensaver,.zshrc,.gitconfig,.gitignore} ./')
-
-
-# command="echo hi"
-
-# for i in "${commands[@]}"
-# do
-#   command="$command; cd; $i"
-# done
-
-# ssh $host $command
+case "$1" in
+    prepare)
+        knife solo prepare $SERVER nodes/workstation.json
+        ;;
+    cook)
+        knife solo cook $SERVER nodes/workstation.json
+        ;;
+    all)
+        $0 prepare
+        $0 cook
+        ;;
+    *)
+        usage
+        ;;
+esac

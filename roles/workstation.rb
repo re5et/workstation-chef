0b8354684315c013302c9505255c9208c4005704
re5et <re5etsmyth@gmail.com>
2015-02-05 15:27:27 -0800

    update workstation role

--- roles/workstation.rb
+++ roles/workstation.rb
@@ -1,25 +1,27 @@
+
 name "server"
 description "the simple, bare necessities"
 
 run_list(
-  "recipe[mysql::client]",
-  "recipe[mysql::server]",
   "recipe[mongodb::10gen_repo]",
   "recipe[ubuntu]",
   "recipe[apt]",
+  "recipe[build-essential]",
   "recipe[unattended-upgrades]",
+  "recipe[mysql::client]",
+  "recipe[mysql::server]",
   "recipe[git]",
+  "recipe[ruby_build]",
   "recipe[workstation::packages]",
   "recipe[workstation::commands]",
-  "recipe[ruby_build]",
   "recipe[workstation::ruby]"
-  )
+)
 
 default_attributes({
-    :ruby => '1.9.3-p448',
-    :mysql => {
-      :server_root_password => '',
-      :server_debian_password => '',
-      :server_repl_password => ''
-    }
-  })
+                     :ruby => '2.1.5',
+                     :mysql => {
+                       :server_root_password => '',
+                       :server_debian_password => '',
+                       :server_repl_password => ''
+                     }
+                   })

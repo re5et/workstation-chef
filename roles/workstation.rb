be7cba3d5c701fa0d0c34218ea6b9572f5b0b3a0
re5et <re5etsmyth@gmail.com>
2015-02-05 16:38:22 -0800

    compile emacs from source

--- roles/workstation.rb
+++ roles/workstation.rb
@@ -3,15 +3,16 @@ name "server"
 description "the simple, bare necessities"
 
 run_list(
-  "recipe[mongodb::10gen_repo]",
   "recipe[ubuntu]",
   "recipe[apt]",
   "recipe[build-essential]",
   "recipe[unattended-upgrades]",
+  "recipe[git]",
   "recipe[mysql::client]",
   "recipe[mysql::server]",
-  "recipe[git]",
+  "recipe[mongodb::10gen_repo]",
   "recipe[ruby_build]",
+  "recipe[emacs24::compile]",
   "recipe[workstation::packages]",
   "recipe[workstation::commands]",
   "recipe[workstation::ruby]"
@@ -23,5 +24,29 @@ default_attributes({
                        :server_root_password => '',
                        :server_debian_password => '',
                        :server_repl_password => ''
+                     },
+                     :emacs24 => {
+                       :packages => [
+                         "libc6-dev",
+                         "libjpeg62-dev",
+                         "libncurses5-dev",
+                         "libpng-dev",
+                         "libtiff4-dev",
+                         "libgif-dev",
+                         "xaw3dg-dev",
+                         "zlib1g-dev",
+                         "libice-dev",
+                         "libsm-dev",
+                         "libx11-dev",
+                         "libxext-dev",
+                         "libxi-dev",
+                         "libxmu-dev",
+                         "libxmuu-dev",
+                         "libxpm-dev",
+                         "libxrandr-dev",
+                         "libxt-dev",
+                         "libxtst-dev",
+                         "libxv-dev"
+                       ]
                      }
                    })

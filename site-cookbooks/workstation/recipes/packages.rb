2efed0418481b894a83d280a2fbef0bb58b99f61
re5et <re5etsmyth@gmail.com>
2015-02-05 16:10:03 -0800

    simpler packages

--- site-cookbooks/workstation/recipes/packages.rb
+++ site-cookbooks/workstation/recipes/packages.rb
@@ -1,19 +1,28 @@
-apt_repository "emacs24" do
-  uri "http://ppa.launchpad.net/cassou/emacs/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  key "CEC45805"
-  keyserver "hkp://keyserver.ubuntu.com:80"
-  action :add
-end
-
-%w{ emacs24 emacs24-el emacs24-common-non-dfsg}.each do |pkg|
-  package pkg do
-    action :install
-  end
-end
-
-%w{linux-headers-generic autoconf synergy curl git nmap zsh xscreensaver electricsheep dmenu texinfo sbcl cl-clx-sbcl cl-ppcre sakura ncurses-term acpi virtualbox sshfs chromium-browser avahi-daemon avahi-discover imagemagick}.each do |pkg|
+%w{
+ubuntu-desktop
+linux-headers-generic
+autoconf
+synergy
+curl
+git
+nmap
+zsh
+xscreensaver
+dmenu
+texinfo
+sbcl
+cl-clx-sbcl
+cl-ppcre
+sakura
+ncurses-term
+acpi
+virtualbox
+sshfs
+chromium-browser
+avahi-daemon
+avahi-discover
+imagemagick
+}.each do |pkg|
   package pkg do
     action :install
   end
